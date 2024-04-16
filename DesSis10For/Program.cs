09962146a343b040a70801fb3372c8462837be71
Usuario <Usuario@CPD02-M04>
2024-04-16 11:42:12 -0300

    Adicionar arquivos de projeto.

---
+++ DesSis10For/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesSis10For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a =  1; a <= 9; a++) 
+            {
+
+                Console.Write(a);
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (a != j)
+                    {
+                    Console.Write(j);
+                    }
+
+                }
+                Console.WriteLine("");
+            }
+
+               Console.ReadKey();
+        }
+    }
+}
